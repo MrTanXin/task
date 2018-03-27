3fdf8040f721181f5a43e5644aa5cc4f7ca5d76a
谈 欣 <tanx990512@outlook.com>
2018-03-27 13:08:31 +0800

    添加源码

---
+++ C_task/1-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("******************************\n");
+	printf("Num:你的学号\n");
+	printf("Name:你的姓名(汉语拼音)\n");
+	printf("******************************\n");
+	return 0;
+}
