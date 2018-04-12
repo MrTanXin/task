5ee7e2359f8a2c55ff8d0a751dbeb28c6f1285a6
谈 欣 <tanx990512@outlook.com>
2018-04-12 14:30:44 +0800

    添加源码

--- maintask/29-1.c
+++ maintask/29-1.c
@@ -4,7 +4,8 @@
 int main()
 {
 	int h;
-	float *p = NULL;
+	float p_max;
+	float *p = &p_max;
 	int i, j;
 	float score[6][5];
 
@@ -27,26 +28,26 @@ int main()
 	{
 		for ( j = 0; j < 5; j++)
 		{
-			printf("%f ", score[i][j]);
+			printf("%.0f ", score[i][j]);
 		}
 		printf("\n");
 	}
 
 	printf("\n");
-	printf("第%d行最大", h);
+	printf("第%d行最大:%.0f", h,p_max);
 
 }
 
 int findmax(float s[][5], float *p)
 {
 	int i;
-	p = &s[0][4];
+	*p = s[0][4];
 	int h = 0;
 	for ( i = 0; i < 6; i++)
 	{
 		if (*p < s[i][4])
 		{
-			p = &s[i][4];
+			*p = s[i][4];
 			h = i + 1;
 		}
 	}
